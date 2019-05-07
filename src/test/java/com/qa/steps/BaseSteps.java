4b932f5db8282ddac001ed165d003ac38c76175c
AbasSunday <abas.sundays@gmail.com>
2019-05-07 23:49:36 +0100

    Fixed framework + implemented sample scenario + cleanup.

---
+++ src/test/java/com/qa/steps/BaseSteps.java
@@ -0,0 +1,9 @@
+package com.qa.steps;
+
+import com.qa.base.SeleniumDriver;
+
+public class BaseSteps {
+
+    protected SeleniumDriver driver = SeleniumDriver.getInstance();
+
+}
---
+++ src/test/java/com/qa/steps/MainPageSteps.java
@@ -0,0 +1,9 @@
+package com.qa.steps;
+
+public class MainPageSteps extends BaseSteps{
+
+
+    public void test1(){
+
+    }
+}
